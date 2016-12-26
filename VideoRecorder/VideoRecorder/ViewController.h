8a46932abc3a7bd9c80e214e6c33bdbfce70b400
felix <2319979647@qq.com>
2016-12-26 15:42:50 +0800

    init project

---
+++ VideoRecorder/VideoRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoRecorder
+//
+//  Created by felix on 2016/12/26.
+//  Copyright © 2016年 felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
